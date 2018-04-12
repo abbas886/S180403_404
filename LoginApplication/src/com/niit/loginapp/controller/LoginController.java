06b4804363276002e632c77c450181ff605ba7a9
Abbas <abbas886@yahoo.com>
2018-04-12 10:34:09 +0530

    started login app without frameworks'

---
+++ LoginApplication/src/com/niit/loginapp/controller/LoginController.java
@@ -0,0 +1,56 @@
+package com.niit.loginapp.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// access the values which entered in id and password field
+		String id = request.getParameter("userID");
+		String pwd =  request.getParameter("password");
+
+		RequestDispatcher  dispatcher;
+	
+		// tempororiy assuming the id : niit and password:niit@123
+		// are valid credentials. - later will intergate with db
+		if (id.equals("niit") && pwd.equals("niit@123")) {
+			dispatcher =request.getRequestDispatcher("home");
+			dispatcher.forward(request, response);
+		} else {
+			dispatcher =request.getRequestDispatcher("login");
+			dispatcher.forward(request, response);
+			//show error message - will discuss tomorrow.
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 1 copy the code from doGet and paste here.
+		// OR 2 call doGet method from doPost method
+
+		doGet(request, response);
+
+	}
+
+}
